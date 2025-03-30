409c4cbd7acd2b28c529b1dc73d6dd6602feb67c
dzianis.haurylavets <dzianis.haurylavets@gmail.com>
2025-03-31 02:23:22 +0300

    changed model

--- ai_model.py
+++ ai_model.py
@@ -1,14 +1,19 @@
 from transformers import BertTokenizer, BertModel
 
 print("Setting-up AI Model")
-model_name = 'bert-base-multilingual-cased'
+model_name = 'bert-base-multilingual-uncased'
 tokenizer = BertTokenizer.from_pretrained(model_name)
 model = BertModel.from_pretrained(model_name)
 model.eval()  # set model to eval mode?
 
 
+# model_name = 'KoichiYasuoka/roberta-small-belarusian'
+# tokenizer=AutoTokenizer.from_pretrained(model_name)
+# model=AutoModelForMaskedLM.from_pretrained(model_name)
+
+
 def get_embedding(text):
-    inputs = tokenizer(text, return_tensors='pt', padding=True, truncation=True, max_length=40)
+    inputs = tokenizer(text.lower(), return_tensors='pt', padding=True, truncation=True, max_length=2048)
     outputs = model(**inputs)
     torch_embeddings = outputs.last_hidden_state[:, 0, :]
     return torch_embeddings.detach().cpu().numpy()[0].tolist()
